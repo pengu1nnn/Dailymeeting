5eae6379510f900748e16734d3cc092ac5b5ef24
turmunkh <turmunkh.p@ebazaar.mn>
2026-08-19 14:36:40 +0800

    faq updated

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -5,6 +5,7 @@
         <p>2. DM -ийн хугацаанд тойрч суугаад, идэвхтэй оролцох</p>
         <p>3. Хагас жилийн эцэст DM бүртгэлээс хамаарч хувьлан KPI үнэлгээнд оруулна.</p>
         <p>4. <a href="https://dailymeeting-five.vercel.app/">https://dailymeeting-five.vercel.app/</a>   - 7 хоног бүр 1 хүн сонгон DM бүртгэлээ хийнэ. </p>
+        <p>5. Хоёр дахь өдөр DM хийхгүй.</p>
     </div>
 </template>
 
